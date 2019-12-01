ae3cef7c3a619ef1b1e3be390831485bafb95d23
Baili Deng <rayman0411@gmail.com>
2019-12-01 16:51:17 +0800

    Finished basic camera rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     public UnityEngine.Transform player;
     Vector3 offset;
+    Vector2 mLook;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +17,16 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        Quaternion rotation = Quaternion.Euler (mLook.y, mLook.x, 0);
+        offset = rotation*offset;
         transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+
+    public void OnLook(InputAction.CallbackContext context){
+        mLook = context.ReadValue<Vector2>();
+        //Debug.Log(context.ReadValue<Vector2>().ToString());
+        Debug.Log(mLook.ToString());
+       // Debug.Log("WTF?");
     }
 }
